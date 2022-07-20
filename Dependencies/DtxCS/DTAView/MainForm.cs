180f0ff2c92b1979bec290630e3c05a9bf9a5720
LysiX <smyler02@gmail.com>
2022-07-19 20:23:55 -0700

    Changes to be committed: 	new file:   Dependencies/DtxCS/.gitattributes 	new file:   Dependencies/DtxCS/.gitignore 	new file:   Dependencies/DtxCS/DTAView/App.config 	new file:   Dependencies/DtxCS/DTAView/DTAView.csproj 	new file:   Dependencies/DtxCS/DTAView/MainForm.Designer.cs 	new file:   Dependencies/DtxCS/DTAView/MainForm.cs 	new file:   Dependencies/DtxCS/DTAView/MainForm.resx 	new file:   Dependencies/DtxCS/DTAView/Program.cs 	new file:   Dependencies/DtxCS/DTAView/Properties/AssemblyInfo.cs 	new file:   Dependencies/DtxCS/DTAView/Properties/Resources.Designer.cs 	new file:   Dependencies/DtxCS/DTAView/Properties/Resources.resx 	new file:   Dependencies/DtxCS/DTAView/Properties/Settings.Designer.cs 	new file:   Dependencies/DtxCS/DTAView/Properties/Settings.settings 	new file:   Dependencies/DtxCS/DTAView/StreamExtensions.cs 	new file:   Dependencies/DtxCS/DTXTool/App.config 	new file:   Dependencies/DtxCS/DTXTool/DTXTool.cs 	new file:   Dependencies/DtxCS/DTXTool/DTXTool.csproj 	new file:   Dependencies/DtxCS/DTXTool/Properties/AssemblyInfo.cs 	new file:   Dependencies/DtxCS/DtxCS.sln 	new file:   Dependencies/DtxCS/Library/Builtins.cs 	new file:   Dependencies/DtxCS/Library/CryptStream.cs 	new file:   Dependencies/DtxCS/Library/DTX.cs 	new file:   Dependencies/DtxCS/Library/DataTypes/ArrayTypes.cs 	new file:   Dependencies/DtxCS/Library/DataTypes/AtomicTypes.cs 	new file:   Dependencies/DtxCS/Library/DataTypes/Directives.cs 	new file:   Dependencies/DtxCS/Library/DataTypes/Types.cs 	new file:   Dependencies/DtxCS/Library/DtxCS.csproj 	new file:   Dependencies/DtxCS/Library/Properties/AssemblyInfo.cs 	new file:   Dependencies/DtxCS/Library/StreamExtensions.cs 	new file:   Dependencies/DtxCS/README.md

---
+++ Dependencies/DtxCS/DTAView/MainForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DtxCS.DataTypes;
+using System.IO;
+
+namespace DTAView
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void textBox1_DragDrop(object sender, DragEventArgs e)
+    {
+      DataArray d;
+      string filename = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+      using (var s = new FileStream(filename, FileMode.Open))
+      {
+        try
+        {
+          d = DtxCS.DTX.FromDtb(s);
+        } catch (Exception)
+        {
+          s.Position = 0;
+          d = DtxCS.DTX.FromDtaStream(s);
+        }
+        var sb = new StringBuilder();
+        foreach (var x in d.Children)
+        {
+          sb.AppendLine(x.ToString(0));
+        }
+        textBox1.Text = sb.ToString();
+      }
+    }
+
+    private void Form1_DragEnter(object sender, DragEventArgs e)
+    {
+      if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        e.Effect = DragDropEffects.Copy; // Okay
+      else
+        e.Effect = DragDropEffects.None; // Unknown data, ignore it
+    }
+
+    private void replInput_KeyPress(object sender, KeyPressEventArgs e)
+    {
+      if(e.KeyChar == (char)Keys.Enter)
+      {
+        replOutput.AppendText(">>> " + replInput.Text + Environment.NewLine);
+        try {
+          var input = DtxCS.DTX.FromDtaString(replInput.Text);
+          foreach (DtxCS.DataTypes.DataNode c in input.Children)
+          {
+            replOutput.AppendText(c.Evaluate().ToString() + Environment.NewLine);
+          }
+        }
+        catch(Exception ex)
+        {
+          replOutput.AppendText(ex.Message + Environment.NewLine);
+        }
+        replOutput.ScrollToCaret();
+        replInput.Clear();
+      }
+    }
+  }
+}
---
+++ Dependencies/DtxCS/DTXTool/DTXTool.cs
@@ -0,0 +1,107 @@
+﻿using DtxCS;
+using DtxCS.DataTypes;
+using System;
+using System.IO;
+using System.Text;
+
+namespace DTXTool
+{
+  internal class DTXTool
+  {
+    static void PrintUsage()
+    {
+      Console.WriteLine();
+      Console.WriteLine("usage:");
+      Console.WriteLine("- dta2b [source .dta] [desination .dtb] [version] (--encrypt)");
+      Console.WriteLine("  converts a text DTA file to a binary DTB/*_DTA_* file of specified version (1, 2 or 3), and optionally encrypt");
+      Console.WriteLine("- dtb2a [source .dtb] [desination .dta]");
+      Console.WriteLine("  converts a binary DTB/*_DTA_* file to a text DTA file");
+      Console.WriteLine("- dtb2b [source .dtb] [desination .dtb] [version] (--encrypt)");
+      Console.WriteLine("  performs an operation on binary DTB/*_DTA_* files, e.g. version change or (de|en)cryption");
+    }
+
+    static void Main(string[] args)
+    {
+      int version = 1;
+      bool encrypt = false;
+      if (args.Length == 0)
+      {
+        PrintUsage();
+        return;
+      }
+      switch (args[0])
+      {
+        case "dta2b":
+          if (args.Length != 4 && args.Length != 5)
+          {
+            PrintUsage();
+            break;
+          }
+          version = int.Parse(args[3]);
+          if (args.Length == 5 && (args[4] == "--encrypt" || args[4] == "-e")) encrypt = true;
+          using (FileStream fi = new FileStream(args[1], FileMode.Open))
+          using (FileStream fo = new FileStream(args[2], FileMode.OpenOrCreate))
+          {
+            DataArray a = DTX.FromDtaStream(fi);
+            // set output to suitable size
+            fo.SetLength(fi.Length * 3);
+            fi.Close();
+            long size = DTX.ToDtb(a, fo, version, encrypt);
+            fo.SetLength(size + (encrypt ? 4 : 0)); // size + 4 extra bytes for key
+            fo.Close();
+          }
+          break;
+        case "dtb2a":
+          if (args.Length != 3)
+          {
+            PrintUsage();
+            break;
+          }
+          using (FileStream fi = new FileStream(args[1], FileMode.Open))
+          using (FileStream fo = new FileStream(args[2], FileMode.OpenOrCreate))
+          {
+            bool encrypted = false;
+            version = DTX.DtbVersion(fi, ref encrypted);
+            Console.WriteLine($"DTB Version {version} {(encrypted ? "(Encrypted)" : "")}");
+            fi.Position = 0;
+            DataArray a = DTX.FromDtb(fi);
+            fi.Close();
+            var sb = new StringBuilder();
+            foreach (var x in a.Children)
+            {
+              sb.AppendLine(x.ToString(0));
+            }
+            byte[] dta = Encoding.UTF8.GetBytes(sb.ToString());
+            fo.Write(dta, 0, dta.Length);
+            fo.Close();
+          }
+          break;
+        case "dtb2b":
+          if (args.Length != 4 && args.Length != 5)
+          {
+            PrintUsage();
+            break;
+          }
+          int targetVersion = int.Parse(args[3]);
+          if (args.Length == 5 && (args[4] == "--encrypt" || args[4] == "-e")) encrypt = true;
+          using (FileStream fi = new FileStream(args[1], FileMode.Open))
+          using (FileStream fo = new FileStream(args[2], FileMode.OpenOrCreate))
+          {
+            bool encrypted = false;
+            version = DTX.DtbVersion(fi, ref encrypted);
+            Console.WriteLine($"DTB Version {version} {(encrypted ? "(Encrypted)" : "")}");
+            fi.Position = 0;
+            DataArray a = DTX.FromDtb(fi);
+            fi.Close();
+            if (targetVersion == 0) targetVersion = version;
+            DTX.ToDtb(a, fo, targetVersion, encrypt);
+            fo.Close();
+          }
+          break;
+        default:
+          PrintUsage();
+          break;
+      }
+    }
+  }
+}
